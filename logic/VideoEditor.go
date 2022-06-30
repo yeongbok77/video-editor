43e4cec262ab2a0cbe31ef04a95e4c57d73140fc
yeongbok77 <2965377095@qq.com>
2022-06-30 15:51:59 +0800

    feat: 视频剪辑功能开发
    实现根据 videoURL、起始终止时间剪辑视频的功能。

--- logic/VideoEditor.go
+++ logic/VideoEditor.go
@@ -3,21 +3,25 @@ package logic
 import (
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 	"log"
+	"strings"
+)
+
+var (
+	tmpVideoPATH string = ""
+	newVideoPATH string = ""
 )
 
 func VideoEditor(videoURL, StartTime, EndTime string) (ResultVideoURL string, err error) {
-	//
-	videoURL = "http://re8twfm01.hd-bkt.clouddn.com/original/WeChat_20220527235010.mp4"
-	StartTime = "00:00:05"
-	EndTime = "00:00:11"
 
-	//
+	splitURL := strings.Split(videoURL, "/")
+	fileName := splitURL[len(splitURL)-1]
+
 	err = ffmpeg.Input(videoURL, ffmpeg.KwArgs{}).
-		Output("D:\\GO_WORK\\src\\video-editor\\public\\tmp\\tmpWeChat_20220527235010.mp4", ffmpeg.KwArgs{"strict": -2, "qscale": 0, "intra": ""}).OverWriteOutput().Run()
+		Output(tmpVideoPATH+fileName, ffmpeg.KwArgs{"strict": -2, "qscale": 0, "intra": ""}).OverWriteOutput().Run()
 
 	// 剪辑
-	err = ffmpeg.Input("D:\\GO_WORK\\src\\video-editor\\public\\tmp\\tmpWeChat_20220527235010.mp4", ffmpeg.KwArgs{}).
-		Output("D:\\GO_WORK\\src\\video-editor\\public\\new\\newWeChat_20220527235010.mp4", ffmpeg.KwArgs{"ss": StartTime, "to": EndTime, "c": "copy"}).OverWriteOutput().Run()
+	err = ffmpeg.Input(tmpVideoPATH+fileName, ffmpeg.KwArgs{}).
+		Output(newVideoPATH+fileName, ffmpeg.KwArgs{"ss": StartTime, "to": EndTime, "c": "copy"}).OverWriteOutput().Run()
 	if err != nil {
 		log.Println("剪辑错误", err)
 
